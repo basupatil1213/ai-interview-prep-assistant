ce962f8c73312a937b771a1ddabc0376dd474d39
Basavaraj Patil <patil.ba@northeastern.edu>
2025-08-13 15:44:54 -0400

    Update AkkaConfiguration for proper ActorSystem bean and cluster settings

--- src/main/java/com/interviewprep/config/AkkaConfiguration.java
+++ src/main/java/com/interviewprep/config/AkkaConfiguration.java
@@ -1,4 +1,18 @@
 package com.interviewprep.config;
 
+import akka.actor.typed.ActorSystem;
+import com.interviewprep.actors.ClusterManager;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AkkaConfiguration {
+
+    @Bean
+    public ActorSystem<ClusterManager.Command> actorSystem() {
+        Config config = ConfigFactory.load();
+        return ActorSystem.create(ClusterManager.create(), "InterviewCluster", config);
+    }
 }
