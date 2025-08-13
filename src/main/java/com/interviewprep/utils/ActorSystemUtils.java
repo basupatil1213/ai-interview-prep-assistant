39a155dff0b24b20a01242cdc9049262afea7665
Basavaraj Patil <patil.ba@northeastern.edu>
2025-08-13 15:45:44 -0400

    Update ActorSystemUtils for Akka actor system utilities

--- src/main/java/com/interviewprep/utils/ActorSystemUtils.java
+++ src/main/java/com/interviewprep/utils/ActorSystemUtils.java
@@ -1,4 +1,15 @@
 package com.interviewprep.utils;
 
 public class ActorSystemUtils {
+
+    public static void logActorMessage(String actorName, String message) {
+        System.out.println(String.format("[%s] %s: %s",
+                java.time.LocalTime.now().toString(),
+                actorName,
+                message));
+    }
+
+    public static void logClusterEvent(String event) {
+        System.out.println(String.format("🔗 [CLUSTER] %s", event));
+    }
 }
